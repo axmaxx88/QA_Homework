c0af1ae86add9810bec3d9d12c4a666b7a62ea1a
Максим Аксенов <ax_maxx@mail.ru>
2022-07-10 11:28:06 +0400

    Add SQL file with creating data script

---
+++ T_SQL/create_data.sql
@@ -0,0 +1,31 @@
+--Если Вы создавали таблицы Goods и Categories на предыдущем уровне курса, то их предварительно нужно удалить.
+--Создание таблицы
+CREATE TABLE Goods (
+	ProductId INT IDENTITY(1,1) NOT NULL,
+	Category INT NOT NULL,
+	ProductName VARCHAR(100) NOT NULL,
+	ProductDescription VARCHAR(100) NULL,
+	Price MONEY NULL,
+	OrderDate DATETIME NULL
+);
+
+--Добавление данных в таблицу
+INSERT INTO Goods(Category, ProductName, ProductDescription, Price, OrderDate)
+	VALUES (1, 'Системный блок ', NULL, 50, '01.12.2019 08:20:30'),
+		   (1, ' Клавиатура', 'Подлежит ремонту',  30, '10.01.2020 09:00:00'),
+		   (1, 'Монитор', NULL, 100, '05.02.2020 13:20:55'),
+		   (2, 'Планшет  ',  NULL, 150, '15.03.2020 20:18:20'),
+		   (2, '  Смартфон', 'Хорошее соотношение цена качество', 100,  '24.04.2020 11:30:45'),
+		   (2, '123456789', NULL, 300, '18.05.2020 18:23:32');
+
+--Создание таблицы с категориями
+CREATE TABLE Categories (
+	CategoryId INT IDENTITY(1,1) NOT NULL,
+	CategoryName VARCHAR(100) NOT NULL
+);
+
+--Добавление данных в таблицу с категориями
+INSERT INTO Categories
+	VALUES ('Комплектующие компьютера'),
+		   ('Мобильные устройства');
+		   
