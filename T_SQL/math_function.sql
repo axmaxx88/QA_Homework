8ae77750edd3eb8f2e574fcd9bdbc24e006b5700
Максим Аксенов <ax_maxx@mail.ru>
2022-07-12 12:40:41 +0400

    Add math_function file

---
+++ T_SQL/math_function.sql
@@ -0,0 +1,9 @@
+﻿--Example of using math function
+SELECT ABS(-100) AS [ABS],
+	   ROUND(1.567, 2) AS [ROUND],
+	   CEILING(1.6) AS [CEILING],
+	   FLOOR(1.6) AS [FLOOR],
+	   SQRT(16) AS [SQRT],
+	   SQUARE(4) AS [SQUARE],
+	   POWER(4, 2) AS [POWER],
+	   LOG(10) AS [LOG]
