356a7a878445c3689b64fb109dd5a8467827fb0f
Максим Аксенов <ax_maxx@mail.ru>
2023-02-05 15:22:28 +0400

    Adding API for test 'test_google_maps_api'

---
+++ API_Python/utils/api.py
@@ -0,0 +1,36 @@
+from utils.http_methods import HttpMethods
+
+"""Methods for testing Google maps API"""
+
+base_url = "https://rahulshettyacademy.com"     # base URL
+key = "?key=qaclick123"                         # parameter for all requests
+
+
+class GoogleMapsAPI:
+
+    """Method for creating new location"""
+    @staticmethod
+    def create_new_place():
+        json_for_creating_new_place = {
+            "location": {
+                "lat": -38.383494,
+                "lng": 33.427362
+            }, "accuracy": 50,
+            "name": "Frontline house",
+            "phone_number": "(+91) 983 893 3937",
+            "address": "29, side layout, cohen 09",
+            "types": [
+                "shoe park",
+                "shop"
+            ],
+            "website": "http://google.com",
+            "language": "French-IN"
+        }
+
+        post_resource = "/maps/api/place/add/json"      # Post method resource
+        post_url = base_url + post_resource + key
+        print(post_url)
+        result_post = HttpMethods.post(post_url, json_for_creating_new_place)
+        print(result_post.text)
+        return result_post
+
