e154d814c66a8da03ca500b7764c3f75806c8de3
axmax <axmaxx88@gmail.com>
2023-06-11 17:01:04 +0400

    testing commit from pycharm

--- API_Python/utils/http_methods.py
+++ API_Python/utils/http_methods.py
@@ -1,4 +1,5 @@
 import requests
+import allure
 from utils.logger import Logger
 
 """HTTP Method list"""
@@ -10,28 +11,32 @@ class HttpMethods:
 
     @staticmethod
     def get(url):
-        Logger.add_request(url, method="GET")
-        result = requests.get(url, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
-        Logger.add_response(result)
-        return result
+        with allure.step("GET"):
+            Logger.add_request(url, method="GET")
+            result = requests.get(url, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
+            Logger.add_response(result)
+            return result
 
     @staticmethod
     def post(url, body):
-        Logger.add_request(url, method="POST")
-        result = requests.post(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
-        Logger.add_response(result)
-        return result
+        with allure.step("POST"):
+            Logger.add_request(url, method="POST")
+            result = requests.post(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
+            Logger.add_response(result)
+            return result
 
     @staticmethod
     def put(url, body):
-        Logger.add_request(url, method="PUT")
-        result = requests.put(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
-        Logger.add_response(result)
-        return result
+        with allure.step("PUT"):
+            Logger.add_request(url, method="PUT")
+            result = requests.put(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
+            Logger.add_response(result)
+            return result
 
     @staticmethod
     def delete(url, body):
-        Logger.add_request(url, method="DELETE")
-        result = requests.delete(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
-        Logger.add_response(result)
-        return result
+        with allure.step("DELETE"):
+            Logger.add_request(url, method="DELETE")
+            result = requests.delete(url, json=body, headers=HttpMethods.headers, cookies=HttpMethods.cookie)
+            Logger.add_response(result)
+            return result
